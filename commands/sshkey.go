882005f66c26b674cc79434c473a5173a7be26dc
Stephan Florquin <stephan.florquin@gmail.com>
2014-12-13 16:12:25 +0100

    Added script specific fields.

--- commands/sshkey.go
+++ commands/sshkey.go
@@ -7,43 +7,98 @@ import (
 	"github.com/stephan83/vultrapi/requests"
 	"io"
 	"text/tabwriter"
+	"flag"
 )
 
-type sshKey struct{ BasicCommand }
-
-func NewSSHKey() Command {
-	return &sshKey{
-		BasicCommand{
-			Desc:        "Get server information.",
-			NeedsKey:    true,
-			ArgsDesc:    "ssh_key_id",
-			OptionsDesc: "Get SSH key information.",
-		},
-	}
+type sshKey struct{
+	BasicCommandWithOptions
+	Labels bool
+	Fields StringSlice
 }
 
-func (_ *sshKey) Fexec(w io.Writer, c Client, args []string, key string) (err error) {
+func NewSSHKey() Command {
+	f := flag.NewFlagSet("sshkey", flag.ContinueOnError)
+
+	o := sshKey{
+		BasicCommandWithOptions: BasicCommandWithOptions{
+			BasicCommand{
+				Desc:     "Get SSH key information.",
+				NeedsKey: true,
+				ArgsDesc: "ssh_key_id",
+			},
+			f,
+		},
+	}
+
+	f.BoolVar(&o.Labels, "labels", true, "display labels")
+	f.Var(&o.Fields, "field", "only display the specified field (can be set multiple times)")
+
+	o.Initialize()
+
+	o.OptionsDesc += "\nAvailable fields: id, name, date_created, key"
+
+	return &o
+}
+
+func (o *sshKey) Fexec(w io.Writer, c Client, args []string, key string) (err error) {
 	if len(args) < 1 {
 		err = ErrUsage{}
 		return
 	}
+
+	o.FlagSet.SetOutput(w)
+
+	err = o.FlagSet.Parse(args[1:])
+	if err != nil {
+		return ErrUsage{}
+	}
+
 	r, err := requests.GetSSHKeys(c, key)
 	if err != nil {
 		return
 	}
 
-	o, ok := r[args[0]]
+	e, ok := r[args[0]]
 	if !ok {
 		err = ErrNotFound{}
 		return
 	}
 
+	fields := map[string]bool{}
+	for _, v := range o.Fields {
+		fields[v] = true
+	}
+
 	t := tabwriter.NewWriter(w, 0, 8, 1, '\t', 0)
 
-	fmt.Fprintf(t, "ID\t%s\n", o.Id)
-	fmt.Fprintf(t, "NAME\t%s\n", o.Name)
-	fmt.Fprintf(t, "DATE CREATED\t%s\n", o.DateCreated)
-	fmt.Fprintf(t, "KEY\t%s\n", o.Key)
+	if fields["id"] || len(o.Fields) == 0 {
+		if o.Labels {
+			fmt.Fprintf(t, "ID\t%s\n", e.Id)
+		} else {
+			fmt.Fprintf(t, "%s\n", e.Id)
+		}
+	}
+	if fields["name"] || len(o.Fields) == 0 {
+		if o.Labels {
+			fmt.Fprintf(t, "NAME\t%s\n", e.Name)
+		} else {
+			fmt.Fprintf(t, "%s\n", e.Name)
+		}
+	}
+	if fields["date_created"] || len(o.Fields) == 0 {
+		if o.Labels {
+			fmt.Fprintf(t, "DATE CREATED\t%s\n", e.DateCreated)
+		} else {
+			fmt.Fprintf(t, "%s\n", e.DateCreated)
+		}
+	}
+	if fields["key"] || len(o.Fields) == 0 {
+		if o.Labels {
+			fmt.Fprintf(t, "KEY\t%s\n", e.Key)
+		} else {
+			fmt.Fprintf(t, "%s\n", e.Key)
+		}
+	}
 
 	t.Flush()
 
--- commands/sshkey_test.go
+++ commands/sshkey_test.go
@@ -14,3 +14,11 @@ func ExampleSSHKey() {
 	// DATE CREATED	2014-12-09 23:30:34 +0000
 	// KEY		****
 }
+
+func ExampleSSHKeySpecificField() {
+	c := NewTestClient(200, keys)
+	NewSSHKey().Fexec(os.Stdout, c, []string{"5487861ad6c8a", "-field", "name", "-field", "key"}, "API_KEY")
+	// Output:
+	// NAME	test2
+	// KEY	****
+}
